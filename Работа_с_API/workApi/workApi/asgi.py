7fd8aae485a6b4c098cc3fa5e69ccfc7f93f21f7
Renat <renat.rakimzhanov@mail.ru>
2025-03-08 01:08:45 +0500

    work with api

--- Работа_с_API/workApi/workApi/asgi.py
+++ Работа_с_API/workApi/workApi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for homework1 project.
+ASGI config for workApi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workApi.settings')
 
 application = get_asgi_application()
