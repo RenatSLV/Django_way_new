fd219cc887c5c65d59262248766d759df35d46a7
Renat <renat.rakimzhanov@mail.ru>
2025-03-08 01:28:05 +0500

    add more new views

--- Лист_Задач_Версия_больше_функционала/ListTask/apps.py
+++ Лист_Задач_Версия_больше_функционала/ListTask/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FirstappConfig(AppConfig):
+class ListtaskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firstapp'
+    name = 'ListTask'
